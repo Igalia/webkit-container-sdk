07a7ee842f790db8826f2522efd1d78c00f88aa1
Nikolas Zimmermann <nzimmermann@igalia.com>
2023-01-24 15:54:52 +0100

    Finish scripts/ split, much easier to understand now what is supposed to run where (host/container)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/bash -x
 
 # 1) Build image using podman
-scripts/build.sh
+host_scripts/testing/build-image.sh
 STATUS=$?
 if [[ ${STATUS} -ne 0 ]]; then
     exit ${STATUS}
 fi
 
 # 2) Test creation of quick toolbx containers
-scripts/create-toolbox.sh
+host_scripts/testing/create-toolbox-container.sh
 STATUS=$?
 if [[ ${STATUS} -ne 0 ]]; then
     exit ${STATUS}
 fi
 
 # 3) Test creation of full-fledged distrobox containers
-host_scripts/create-home-directory.sh /tmp/wkdev-bootstrap-home
+host_scripts/testing/create-home-directory.sh /tmp/wkdev-bootstrap-home
 STATUS=$?
 if [[ ${STATUS} -ne 0 ]]; then
     exit ${STATUS}
 fi
 
-scripts/create-distrobox.sh /tmp/wkdev-bootstrap-home
+host_scripts/testing/create-distrobox-container.sh /tmp/wkdev-bootstrap-home
 STATUS=$?
 if [[ ${STATUS} -ne 0 ]]; then
     exit ${STATUS}
@@ -32,4 +32,4 @@ rm -Rf /tmp/wkdev-bootstap-home &> /dev/null
 
 # 5) Show instructions how to deploy new SDK image to docker.io
 echo ""
-echo "Ready. If everything went well, use \"./scripts/deploy.sh\" to push the new SDK image to the registry, once tested!"
+echo "Ready. If everything went well, use \"./host_scripts/testing/deploy-image.sh\" to push the new SDK image to the registry, once tested!"
--- packages/sources/custom/gst-plugins-good1.0/build.sh
+++ packages/sources/custom/gst-plugins-good1.0/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 THIS_FILE=$(basename ${0})
-/wkdev-sdk/build-wkdev-dependencies.sh "$(basename $(echo "${0}" | sed -e "s#${THIS_FILE}##"))"
+/wkdev-sdk/container_scripts/build-wkdev-dependencies.sh "$(basename $(echo "${0}" | sed -e "s#${THIS_FILE}##"))"

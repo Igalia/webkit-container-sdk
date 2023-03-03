884c1ef6b860026b667a2113a862b3696898637c
Nikolas Zimmermann <nzimmermann@igalia.com>
2023-03-03 15:09:12 +0100

    Finish fully automatized build process.
    Build & bundle all necessar packages defined in the two yaml files:
    - wkdev-podman4-ubuntu-kinetic.yaml (podman 4.4.x for Ubuntu 22.10, not
      available from official sources)
    - wkdev-webkit-dependencies: self-compiled WebKit dependencies, such as
      GStreamer, etc.
    
    Create these packages on-the-fly during the wkdev-sdk container image
    file creation. The same procedure can be repeated within the container
    to rebuild selective parts of the SDK, by the user. If the user of the
    SDK only wants to work on WebKit itself, no need to build any SDK
    package, just use the SDK-bundled versions (indicated by 111wkdev111
    suffix). If the user compiles custom variants of e.g. a certain
    gstreamer package, it's automatically indicated by a 222wkdev222 prefix
    to avoid confusion, about the package origin, when working on WebKit
    _dependencies_.

--- auxiliary/ubuntu_package_builder/build-package.sh
+++ auxiliary/ubuntu_package_builder/build-package.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
-trap '[ "$?" -ne 0 ] && printf "[ERROR] A fatal error occurred. Aborting!\n"' EXIT
 
 build_profile=${1}
 package_full_name=${2}
-debuild_options=${3}
+deb_build_options=${3}
+dpkg_buildpackage_options=${4}
 
 package_name=$(basename "${package_full_name}")
 work_directory="/builder/work"
-packages_directory="${work_directory}/packages"
+packages_directory="/builder/packages"
 source_directory="${work_directory}/sources/${package_full_name}/${package_name}"
 build_directory="${work_directory}/builds/${package_full_name}"
 dependencies_file="${build_directory}/.dependencies"
@@ -50,7 +50,7 @@ fi
 printf "\n-> ccache build cache statistics:\n"
 ccache --show-stats --verbose | sed -e "s/^/    /"
 
-printf "\n-> Switch into source directory '${source_directory}'.\n"
+printf "\n-> Switch into source directory '${source_directory}', preparing to build '${package_name}'...\n"
 pushd "${source_directory}" &>/dev/null
 
 # Install build dependencies, as specified in dsc file.
@@ -61,55 +61,40 @@ mk-build-deps --install --remove --tool "${APT_GET} -o Debug::pkgProblemResolver
 printf "\n-> Building package using 'gbp buildpackage'...\n"
 printf "   Build log location: ${build_directory}/build.log\n"
 
-# Choose 'DEB_BUILD_OPTIONS' and 'debuild_options' according to build profile.
-# Build profile: fast (do not generate documentation, do not run tests after build, do not run lintian -- used for _development_ (all packages built _in_ the container are built in 'fast' mode)
-# Build profile: full (generate documentation, run tests after build, run lintian -- used for the self-compiled packages that got created during SDK image creation (all packages built on the host use 'full')
+# Choose 'DEB_BUILD_OPTIONS' and 'DEBUILD_LINTIAN' environment variable values according to build profile.
+# Build profile: fast -> do not generate documentation, do not run tests after build, do not run lintian -- used for _development_ (all packages built _in_ the container are built in 'fast' mode)
+# Build profile: full -> generate documentation, run tests after build, run lintian -- used for the self-compiled packages that got created during SDK image creation (all packages built on the host use 'full')
 DEB_BUILD_OPTIONS_fast="nocheck nodoc"
 DEB_BUILD_OPTIONS_full=""
-DEB_BUILD_OPTIONS_selected=DEB_BUILD_OPTIONS_${build_profile}
-
-use_debuild_options_fast="--no-lintian"
-if [ ! -z "${debuild_options}" ]; then
-    use_debuild_options_fast="${use_debuild_options_fast} ${debuild_options}"
+if [ ! -z "${deb_build_options}" ]; then
+    DEB_BUILD_OPTIONS_selected=deb_build_options
+else
+    DEB_BUILD_OPTIONS_selected=DEB_BUILD_OPTIONS_${build_profile}
 fi
-use_debuild_options_full="${debuild_options}"
-use_debuild_options_selected=use_debuild_options_${build_profile}
+
+DEBUILD_LINTIAN_fast="no"
+DEBUILD_LINTIAN_full="yes"
+DEBUILD_LINTIAN_selected=DEBUILD_LINTIAN_${build_profile}
 
 if [ "${build_profile}" == "fast" ] || [ "${build_profile}" == "full" ]; then
     printf "\n-> Selected build profile '${build_profile}':\n"
     printf "   DEB_BUILD_OPTIONS=\"${!DEB_BUILD_OPTIONS_selected}\" -- passed as environment variable to 'gbp buildpackage'\"\n"
-    printf "   debuild_options=\"${!use_debuild_options_selected}\" -- passed as aditional options to 'debuild'\n\n"
+    printf "   DEBUILD_LINTIAN=\"${!DEBUILD_LINTIAN_selected}\" -- passed as environment variable to 'gbp buildpackage'\"\n"
 else
     printf "\n-> Unknown build profile '${build_profile}'. Please either pass 'fast' or 'full'.\n"
     exit 1
 fi
 
-DEB_BUILD_OPTIONS="${!DEB_BUILD_OPTIONS_selected}" gbp buildpackage \
-    --git-builder="debuild ${!use_debuild_options_selected} --prepend-path='/usr/lib/ccache' --preserve-envvar='CCACHE_*' --no-sign" \
-    --git-export-dir="${build_directory}" --git-no-purge --git-ignore-branch --git-ignore-new --git-pristine-tar-commit &> "${build_directory}/build.log" &
-
-build_pid=${!}
-tail --follow --pid=${build_pid} "${build_directory}/build.log" &
-
-wait ${build_pid}
-build_status=${?}
-
-if [ ${build_status} -ne 0 ]; then
-    printf "\n-> Build failed. Aborting with exit code ${build_status}.\n"
-fi
+DEBUILD_LINTIAN="${!DEBUILD_LINTIAN_selected}" DEB_BUILD_OPTIONS="${!DEB_BUILD_OPTIONS_selected}" gbp buildpackage \
+    --git-builder="debuild --prepend-path='/usr/lib/ccache' --preserve-envvar='CCACHE_*' --no-sign ${dpkg_buildpackage_options}" \
+    --git-export-dir="${build_directory}" --git-no-purge --git-ignore-branch --git-ignore-new
 
 popd &>/dev/null
-
-# Copy packages to final destination
-printf "\n-> Copy build artifacts to packages directory '${packages_directory}'...\n"
-
-pushd "${packages_directory}" &>/dev/null
 
 file_extensions=(ddeb deb)
 for file_extension in "${file_extensions[@]}"; do
-    find ../builds/${package_full_name}/ -type f -name \*.${file_extension} -exec ln --symbolic --force --verbose {} "${packages_directory}"/ \;
+    printf "\n-> Copy *.${file_extension} files from build directory '${build_directory}' to packages directory '${packages_directory}'...\n"
+    find "${build_directory}"/  -type f -name "*.${file_extension}" -exec cp --archive --verbose {} "${packages_directory}"/ \;
 done
-
-popd &>/dev/null
 
 printf "\n-> Finished!\n"
--- auxiliary/ubuntu_package_builder/invoke-build-package.sh
+++ auxiliary/ubuntu_package_builder/invoke-build-package.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 work_directory=${1}
-package_full_name=${2}
-debuild_options=${3}
+packages_directory=${2}
+package_full_name=${3}
+deb_build_options=${4}
+dpkg_buildpackage_options=${5}
+
+build_directory="${work_directory}/builds/${package_full_name}"
 
 # Execute the podman conainer from within the host, not from within the container.
 # While this works fine for same cases, to build debian packages, it's too tricky,
@@ -14,9 +18,25 @@ if [ -f /run/.containerenv ]; then
 
     # Translate from container home relative path to host path.
     work_directory="${work_directory/${HOME}/${HOST_CONTAINER_HOME_PATH}}"
+    packages_directory="${packages_directory/${HOME}/${HOST_CONTAINER_HOME_PATH}}"
 fi
 
 ${podman_executable} run --network host --rm \
        --mount type=bind,source=${work_directory},destination=/builder/work,rslave \
+       --mount type=bind,source=${packages_directory},destination=/builder/packages,rslave \
        --mount type=volume,source=ubuntu-package-builder-22-10-cache,destination=/builder/cache \
-       docker.io/nikolaszimmermann/ubuntu-package-builder:22.10 /builder/build-package.sh "${build_profile}" "${package_full_name}" "${debuild_options}"
+       docker.io/nikolaszimmermann/ubuntu-package-builder:22.10 /builder/build-package.sh \
+       "${build_profile}" "${package_full_name}" "${deb_build_options}" "${dpkg_buildpackage_options}" &> "${build_directory}/build.log" &
+
+build_pid=${!}
+tail --follow --pid=${build_pid} "${build_directory}/build.log" &
+
+wait ${build_pid}
+build_status=${?}
+
+if [ ${build_status} -ne 0 ]; then
+    printf "\n-> Build failed. Aborting with exit code ${build_status}.\n"
+    exit ${build_status}
+fi
+
+printf "\n-> Build finished successfully.\n"

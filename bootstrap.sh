99124baf1cbe70064d4a716d545c873debabdc47
Nikolas Zimmermann <nzimmermann@igalia.com>
2023-03-13 10:16:07 +0100

    Fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-
 test_container_name="wkdev-bootstrap"
 test_home_directory="/tmp/${test_container_name}-home"
 
@@ -13,12 +12,15 @@ scripts/host-only/wkdev-sdk-build --verbose
 
 # 2) Test creation of container
 printf "\n-> Creating '${test_container_name}' container with fresh home directory in '${test_home_directory}'...\n"
-rm -rf "${test_home_directory}" &>/dev/null
+if [ -d "${test_home_directory}" ]; then
+    podman unshare rm -rf "${test_home_directory}" &>/dev/null
+fi
+
 scripts/host-only/wkdev-create --verbose --create-home --home "${test_home_directory}" "${test_container_name}"
 
 # 3) Test entering container
 printf "\n-> Entering '${test_container_name}' container...\n"
-scripts/host-only/wkdev-enter --verbose "${test_container_name}" -e uptime
+scripts/host-only/wkdev-enter --verbose --execute "${test_container_name}" uptime --pretty
 
 # 4) Cleanup
 printf "\n-> Stopping '${test_container_name}' container...\n"
@@ -26,7 +28,7 @@ podman stop "${test_container_name}" &>/dev/null
 
 printf "\n-> Deleting '${test_container_name}' container & home directory...\n"
 podman rm "${test_container_name}" &>/dev/null
-rm -rf "${test_home_directory}" &>/dev/null
+podman unshare rm -rf "${test_home_directory}" &>/dev/null
 
-# 4) Show instructions how to deploy new SDK image to docker.io
+# 5) Show instructions how to deploy new SDK image to docker.io
 printf "\n\nReady. If everything went well, use \"scripts/host-only/wkdev-sdk-deploy\" to push the new SDK image to the registry, once tested!\n"

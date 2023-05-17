30ed1457d7364217d0479d5ed80a03a38f73f73d
Diego Pino García <dpino@igalia.com>
2023-05-17 09:34:39 +0000

    Create 'wkdev' command to use as launcher of other wkdev- commands

---
+++ scripts/.wkdev-completion.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+_wkdev_completions()
+{
+  local dir
+  local words=()
+  
+  # Create array of wkdev- words.
+  dir=$(dirname $(which wkdev))
+  while read each; do
+    each=$(basename "$each")
+    if [[ "$each" == wkdev-* ]]; then
+      words+=(${each:6})
+    fi
+  done <<< $(find $dir -type f -print)
+  
+  # Expand array to string.
+  words=${words[@]}
+  # Set autocomplete words for wkdev.
+  COMPREPLY=($(compgen -W "${words}" "${COMP_WORDS[1]}"))
+} 
+complete -F _wkdev_completions wkdev

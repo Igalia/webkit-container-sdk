03f776cf16d945c4470b5cec047eae583507f67d
Patrick Griffis <pgriffis@igalia.com>
2023-05-19 13:03:57 -0500

    fish: Fix typos

--- register-sdk-on-host.fish
+++ register-sdk-on-host.fish
@@ -1,4 +1,4 @@
 # To be sourced from your e.g. ~/.config/fish/config.fish to integrate wkdev-sdk with your host OS.
 set --export WKDEV_SDK (dirname (readlink -m (status --current-filename)))
-set --global --export --append --path "$WKDEV_SDK/scripts"
-set --global --export --append --path "$WKDEV_SDK/scripts/host-only"
+set --global --export --append --path PATH "$WKDEV_SDK/scripts"
+set --global --export --append --path PATH "$WKDEV_SDK/scripts/host-only"

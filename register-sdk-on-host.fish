f22ed7ab5c0012170f9c8e91a439c2190a0e220a
Patrick Griffis <pgriffis@igalia.com>
2023-07-19 11:03:32 -0500

    fish: Fix possible duplicate PATH entries

--- register-sdk-on-host.fish
+++ register-sdk-on-host.fish
@@ -1,4 +1,4 @@
 # To be sourced from your e.g. ~/.config/fish/config.fish to integrate wkdev-sdk with your host OS.
 set --export WKDEV_SDK (dirname (readlink -m (status --current-filename)))
-set --global --export --append --path PATH "$WKDEV_SDK/scripts"
-set --global --export --append --path PATH "$WKDEV_SDK/scripts/host-only"
+fish_add_path --global --path "$WKDEV_SDK/scripts"
+fish_add_path --global --path "$WKDEV_SDK/scripts/host-only"

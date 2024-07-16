3cfbd3cfbc6178d802d4399c3ebeae2d997c659c
Patrick Griffis <pgriffis@igalia.com>
2024-07-16 11:53:11 +0200

    init: Install asahi drivers automatically

--- utilities/debian-packages.sh
+++ utilities/debian-packages.sh
@@ -7,6 +7,23 @@ verify_executables_exist apt-get dpkg-query
 
 update_packages() { apt-get update; }
 
+add_ppa() {
+
+    local ppa_repo="${1}" # e.g. project/name
+    local name="${2}"
+    local signing_key="${3}"
+    local keyring="/etc/apt/keyrings/${name}.gpg"
+
+    mkdir "${HOME}/.gnupg"
+    dirmngr --daemon
+    gpg --no-default-keyring --keyring="/etc/apt/keyrings/${name}.gpg" --keyserver=keyserver.ubuntu.com --recv-keys "${signing_key}" || _abort_ "Failed to import key for ${ppa_repo}"
+    dirmngr --shutdown
+    rm -r "${HOME}/.gnupg"
+
+    echo "deb [signed-by=${keyring}] http://ppa.launchpad.net/${ppa_repo}/ubuntu noble main
+deb-src [signed-by=${keyring}] http://ppa.launchpad.net/${ppa_repo}/ubuntu noble main" > "/etc/apt/sources.list.d/${name}.list"
+}
+
 is_package_installed() {
 
     local package="${1}"
@@ -18,4 +35,11 @@ ensure_package_installed() {
     local package="${1}"
     is_package_installed "${package}" && return 0
     apt-get --assume-yes install "${package}" || _abort_ "Cannot install package '${package}': executing 'apt-get install' failed"
+}
+
+upgrade_packages() {
+
+    local packages="${@}"
+
+    apt-get upgrade -y "${packages}" || _abort_ "Cannot upgrade packages '${packages}': executing 'apt-get upgrade' failed"
 }

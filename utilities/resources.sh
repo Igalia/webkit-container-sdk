6a05139588ca1af13be310f2bb1c7b0ea7987c0e
Nikolas Zimmermann <nzimmermann@igalia.com>
2024-11-20 23:45:20 +0100

    Get rid of unused functions in utilities/resources.sh

--- utilities/resources.sh
+++ utilities/resources.sh
@@ -4,7 +4,6 @@
 
 # Internal helpers
 _get_sdk_images_directory() { echo "${WKDEV_SDK}/images"; }
-_get_sdk_templates_directory() { echo "$(_get_sdk_images_directory)/wkdev_sdk/custom_built_packages/templates"; }
 
 # Get absolute path to '<wkdev-sdk>/images/<image>' directory, given an image name.
 # The subdirectory name is equal to the image name, except that hyphens transform to
@@ -15,13 +14,4 @@ get_image_directory_by_name() {
     local image_directory="$(_get_sdk_images_directory)/${image_name//-/_}"
     [ ! -d "${image_directory}" ] && _abort_ "Invalid image directory '${image_directory}' for image name '${image_name}'"
     echo "${image_directory}"
-}
-
-# Get absolute path to '<wkdev-sdk>/.../templates/<template-name>.template' file, given a <template-name>.
-get_template_file_by_name() {
-
-    local template_name="${1}"
-    local template_file="$(_get_sdk_templates_directory)/${template_name}.template"
-    [ ! -f "${template_file}" ] && _abort_ "Invalid template file '${template_file}' for template name '${template_name}'"
-    echo "${template_file}"
 }

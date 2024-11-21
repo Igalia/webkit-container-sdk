ccd09feb466a0cf1037e101159417d39d377d0b2
Nikolas Zimmermann <info@nikolaszimmermann.de>
2024-11-21 10:25:22 +0100

    Modify container image for bots/infrastructure migration (#78)
    Contains a few individual changes, needed for the bot/infrastructure
    migration.

--- utilities/resources.sh
+++ utilities/resources.sh
@@ -3,8 +3,10 @@
 [ -z "${application_ready}" ] && { echo "[FATAL] You need to source 'utilities/application.sh' before sourcing this script."; return 1; }
 
 # Internal helpers
-_get_sdk_images_directory() { echo "${WKDEV_SDK}/images"; }
-_get_sdk_templates_directory() { echo "$(_get_sdk_images_directory)/wkdev_sdk/custom_built_packages/templates"; }
+_get_sdk_images_directory() {
+    local default="${WKDEV_SDK}/images";
+    echo "${WKDEV_SDK_IMAGES_DIRECTORY:-"${default}"}";
+}
 
 # Get absolute path to '<wkdev-sdk>/images/<image>' directory, given an image name.
 # The subdirectory name is equal to the image name, except that hyphens transform to
@@ -15,13 +17,4 @@ get_image_directory_by_name() {
     local image_directory="$(_get_sdk_images_directory)/${image_name//-/_}"
     [ ! -d "${image_directory}" ] && _abort_ "Invalid image directory '${image_directory}' for image name '${image_name}'"
     echo "${image_directory}"
-}
-
-# Get absolute path to '<wkdev-sdk>/.../templates/<template-name>.template' file, given a <template-name>.
-get_template_file_by_name() {
-
-    local template_name="${1}"
-    local template_file="$(_get_sdk_templates_directory)/${template_name}.template"
-    [ ! -f "${template_file}" ] && _abort_ "Invalid template file '${template_file}' for template name '${template_name}'"
-    echo "${template_file}"
 }
